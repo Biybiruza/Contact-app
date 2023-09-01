872b4f21340d28cce948d595763b403959c55837
Biybiruza <ibashovabiybi@gmail.com>
2023-09-01 19:44:21 +0500

    commit

--- app/src/main/java/com/biybiruza/contact/add/AddContactActivity.java
+++ app/src/main/java/com/biybiruza/contact/add/AddContactActivity.java
@@ -1,12 +1,15 @@
 package com.biybiruza.contact.add;
 
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -24,21 +27,25 @@ import java.util.List;
 public class AddContactActivity extends AppCompatActivity {
 
     private ActivityAddContactBinding binding;
-    private MyShared myShared;
-    private SharedPreferences sharedPreferences;
-    private SharedPreferences.Editor editor;
-    private int count = 0;
+    private static final String TAG = "Contact_tag";
+    private static final int WRITE_CONTACT_PERMISSION_CODE = 100;
+    private static final int IMAGE_PICK_GALLERY_CODE = 100;
+    private String[] contactPermission;
+
+    private Uri image_uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityAddContactBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        myShared = new MyShared(this, new Gson());
-        sharedPreferences = getSharedPreferences("Count", MODE_PRIVATE);
-        editor = sharedPreferences.edit();
 
-        count = sharedPreferences.getInt("count", 0);
+        binding.ivImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGalleryIntent();
+            }
+        });
 
         binding.btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,25 +58,54 @@ public class AddContactActivity extends AppCompatActivity {
                         binding.etPhoneNumber.getText().toString().isEmpty()){
                     Toast.makeText(AddContactActivity.this, "Enter name and phone number, please", Toast.LENGTH_SHORT).show();
                 } else {
-                    save();
+                    saveContact();
                 }
             }
         });
     }
 
-    private void save() {
-        List<ContactModels> list = new ArrayList<>();
+    private void openGalleryIntent() {
+        //intent to pick image from gallery
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, IMAGE_PICK_GALLERY_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //handle result either image picked or not
+        if (resultCode == RESULT_OK) {
+            if (requestCode == IMAGE_PICK_GALLERY_CODE) {
+                //image picked
+                image_uri = data.getData();
+                //set to imageview
+                binding.ivImg.setImageURI(image_uri);
+            }
+        } else {
+            //cancelled
+            Toast.makeText(this, "Cancelled...", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void saveContact() {
+
+        //input data
+        String name = binding.etName.getText().toString().trim();
+        String surname = binding.etSurname.getText().toString().trim();
+        String phone = binding.etPhoneNumber.getText().toString().trim();
+
+        ArrayList<ContactModels> list = new ArrayList<>();
+
+        //contact id
+        int rawContactId = list.size();
+
+
+
 
         list.add(new ContactModels(binding.etName.getText().toString(), binding.etSurname.getText().toString(),
                 binding.etPhoneNumber.getText().toString()));
 
-        count++;
-        editor.putInt("count", count);
-        editor.apply();
-        myShared.putList("key_"+count, list);
-
-        Toast.makeText(AddContactActivity.this, "key_"+count, Toast.LENGTH_SHORT).show();
-        Toast.makeText(AddContactActivity.this, "Saved", Toast.LENGTH_SHORT).show();
 
         Log.d("TAG", "list: ");
         binding.etName.setText("");
@@ -77,14 +113,8 @@ public class AddContactActivity extends AppCompatActivity {
         binding.etPhoneNumber.setText("");
 
         Intent intent = new Intent(AddContactActivity.this, MainActivity.class);
-        intent.putExtra("key", count);
         startActivity(intent);
         finish();
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        count = sharedPreferences.getInt("count", 0);
-    }
 }
